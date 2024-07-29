e6b85214cb2bb616574a69158cf5d6256e215b40
Abhishek Shankar Choudhary <28715657+proabhishek@users.noreply.github.com>
2024-07-29 20:02:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function highlight() {
     //Write your code here
-
+    // Get all bold elements inside the paragraph
+            const boldElements = document.querySelectorAll('#paragraph b');
+            boldElements.forEach(element => {
+                // Add the highlight class to each bold element
+                element.classList.add('highlight');
+            });
 
 }
 
 
 function return_normal() {
-    //Write your code here
-
-    
+     const boldElements = document.querySelectorAll('#paragraph b');
+            boldElements.forEach(element => {
+                // Remove the highlight class from each bold element
+                element.classList.remove('highlight');
+            });
 }
