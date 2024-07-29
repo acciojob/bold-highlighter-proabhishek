eac6dbc159fc3083073c227f441990dfc4825840
Abhishek Shankar Choudhary <28715657+proabhishek@users.noreply.github.com>
2024-07-29 20:06:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 function highlight() {
-    //Write your code here
-    // Get all bold elements inside the paragraph
-            const boldElements = document.querySelectorAll('#paragraph b');
-            boldElements.forEach(element => {
-                // Add the highlight class to each bold element
-                element.classList.add('highlight');
-            });
+    const strongElements = document.querySelectorAll('#paragraph strong');
+    strongElements.forEach(element => {
+        // Change the color of each strong element to green
+        element.style.color = 'green';
+    });
 
 }
 
 
 function return_normal() {
-     const boldElements = document.querySelectorAll('#paragraph b');
-            boldElements.forEach(element => {
-                // Remove the highlight class from each bold element
-                element.classList.remove('highlight');
-            });
+    const strongElements = document.querySelectorAll('#paragraph strong');
+    strongElements.forEach(element => {
+        // Change the color of each strong element to green
+        element.style.color = 'black';
+    });
 }
